8790495f1289799740a9f549733e3c311e327822
Gurgy <gurgy@emycia.com>
2014-10-09 14:49:34 +0200

    innupp1.Filestructure created
    Used netbeans to build filestructure for "innlämningsuppgift1"

---
+++ inlamningsuppgift1/src/inlamningsuppgift1/Upp2.java
@@ -0,0 +1,22 @@
+/*
+ * 
+ * 
+ * 
+ */
+
+package inlamningsuppgift1;
+
+/**
+ *
+ * @author william
+ */
+public class Upp2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
