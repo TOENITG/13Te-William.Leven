c0f1acb82bcf433e759be44cf8a07600c998674e
Gurgy <gurgy@emycia.com>
2014-10-09 21:15:01 +0200

    innupp1. improved comments
    cleaned up the code a bit and made more detailed comments.

--- inlamningsuppgift1/src/inlamningsuppgift1/Upp1.java
+++ inlamningsuppgift1/src/inlamningsuppgift1/Upp1.java
@@ -1,38 +1,43 @@
-/*
- * 
- * 
- * 
+/**
+ * No licence
  */
 
 package inlamningsuppgift1;
-import javax.swing.JOptionPane;
-import java.text.DateFormatSymbols;
+
+import javax.swing.JOptionPane;         // Interactive panes (JOptionPanes)
+import java.text.DateFormatSymbols;     // Converting int to month string (.getMonth)
 
 /**
  * @author William Leven
  * @teacher ToEn ITG
- * @version 1.0.0 2014-10-09 16:21
+ * @version 1.0.1 2014-10-09 21:00
  */
 public class Upp1 {
 
     public static void main(String[] args) {
         
-        // Asking for I/O and storing in variables
+        // Asking for I/O and saving to variables
         String Name = JOptionPane.showInputDialog("Skriv ditt namn", "Namn");
         String SocNumber = JOptionPane.showInputDialog("Skriv ditt personnummer", "xxxxxx-xxxx");
         String Adress = JOptionPane.showInputDialog("Skriv din adress", "Exempelvägen xx");
         
         // Extracting birth date from SocNumber
+        // - .trim is used to remmove any blanks
+        // - .substring extracts the desired letters
+        // - .replace is used to remove any zeros
+        // - Integer.parseInt converts the string to an int.
         String BirthDate_day = SocNumber.trim().substring(4, 6).replace("0", "");
         int BirthDate_month_int = Integer.parseInt(SocNumber.trim().substring(2, 4).replace("0", ""));
         
-        // turning the month int to a string with the name of the month
-        String BirthDate_month_result = new DateFormatSymbols().getMonths()[BirthDate_month_int-1];
+        // Turning the month int to a string with the name of the month
+        // - DateFormatSymbols().getMonths()[] convert the int to a string holding the month
+        // - -1 @[BirthDate_month_int-1] is used because the method starts counting at zero.
+        String BirthDate_month_name = new DateFormatSymbols().getMonths()[BirthDate_month_int-1];
         
         // Sending message to user
-        JOptionPane.showMessageDialog(null, "Välkommen " + Name + ", då du fyller den " + BirthDate_day + " " + BirthDate_month_result + " kommer vi komma till dig på " + Adress + " och fira dig.", "automatiskt meddelande", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Välkommen " + Name + ", då du fyller den " + BirthDate_day + " " + BirthDate_month_name + " kommer vi komma till dig på " + Adress + " och fira dig.", "automatiskt meddelande", JOptionPane.PLAIN_MESSAGE);
         
-        // stop memory leaks
+        // Closes all open panes when program ends
         System.exit(0);
     }
     
--- inlamningsuppgift1/src/inlamningsuppgift1/Upp2.java
+++ inlamningsuppgift1/src/inlamningsuppgift1/Upp2.java
@@ -1,42 +1,44 @@
-/*
- * 
- * 
- * 
+/**
+ * No licence
  */
 
 package inlamningsuppgift1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import javax.swing.JOptionPane;
+import java.util.ArrayList;         // Dynamic Arrays (ArrayLists)
+import java.util.Arrays;            // Array handling (asList)
+import javax.swing.JOptionPane;     // Interactive panes (JOptionPanes)
 
 /**
  * @author William Leven
  * @teacher ToEn ITG
- * @version 1.0.0
+ * @version 1.0.0 2014-10-09 20:54
  */
 public class Upp2 {
 
     public static void main(String[] args) {
         
-        //seting up variable(s)
+        //Seting up variables
+        //- Using ArrayList to make the array dynamic.
         ArrayList words = new ArrayList();
-        int i = 0;
         int stop = 0;
         
         while ( stop == 0) {
-            // geting input from user
+            // Geting input from user
+            //- .add & .size is used as im working with ArrayLists.
+            //- +1 @(words.size()+1) as array positions start at 0
             words.add(JOptionPane.showInputDialog("Skriv in ord nr. " + (words.size()+1)));
-            //printing out "words"
-            JOptionPane.showMessageDialog(null, Arrays.asList(words), "Dina Ord", JOptionPane.PLAIN_MESSAGE);
-            //making sure the program doesnt overwrite the prev value
-            i ++;
-            // asking if the user wants to add a new value
-            stop = JOptionPane.showConfirmDialog(null, "Vill du lägga till ett ord till?","Igen", JOptionPane.YES_NO_OPTION);
             
+            // Printing out "words"
+            //- Arrays.asList is used to call all values from the array
+            JOptionPane.showMessageDialog(null, Arrays.asList(words), "Dina Ord", JOptionPane.PLAIN_MESSAGE);
+            
+            // Asking if the user wants to contingue in order to give a way out of the loop.
+            //- If answer is no "stop" will take the value of 1
+            stop = JOptionPane.showConfirmDialog(null, "Vill du lägga till ett ord till?","Igen", JOptionPane.YES_NO_OPTION);
         }
+        
+        // Closes all open panes when program ends
         System.exit(0);
-        // stop memory leaks
        
     }
     
