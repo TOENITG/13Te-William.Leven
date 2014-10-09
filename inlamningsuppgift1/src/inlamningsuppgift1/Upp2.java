9fbd47c7b948ad57a986171ce3e89fb62f982ef2
Gurgy <gurgy@emycia.com>
2014-10-09 17:13:49 +0200

    innupp1.uppp2. Setup input
    input method created and looped

--- inlamningsuppgift1/src/inlamningsuppgift1/Upp2.java
+++ inlamningsuppgift1/src/inlamningsuppgift1/Upp2.java
@@ -6,6 +6,8 @@
 
 package inlamningsuppgift1;
 
+import javax.swing.JOptionPane;
+
 /**
  * @author William Leven
  * @teacher ToEn ITG
@@ -15,6 +17,13 @@ public class Upp2 {
 
     public static void main(String[] args) {
         
+        //seting up variable(s)
+        String[] words = new String[5];
+        
+        // geting input from user
+        for (int i=0; i<words.length; i++) {
+            words[i] = JOptionPane.showInputDialog("Skriv in ord nr. " + i+1);
+        }
         /** ToDo:
          * get 5 user input trough loop and store them in array
          * 
