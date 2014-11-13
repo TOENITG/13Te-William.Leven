efc67a8c8a44f44727207e6a036a65472beb64af
Gurgy <gurgy@emycia.com>
2014-11-13 16:29:33 +0100

    session ended
    made alot of work om merge sort

--- SortAndSearch/src/sortandsearch/SortAndSearch.java
+++ SortAndSearch/src/sortandsearch/SortAndSearch.java
@@ -14,13 +14,13 @@ import java.util.*;
  */
 public class SortAndSearch {
     
-    private static int[] randomArray(int size){
+    private static int[] randomArray(int length, int variation){
         
         // Written by tobias and modified by william.
         
-        int[] theArray = new int[size];
-        for (int i=0;i<size;i++){
-            theArray[i] = (int)(Math.random()*size) + 1;
+        int[] theArray = new int[length];
+        for (int i=0;i<length;i++){
+            theArray[i] = (int)(Math.random()*variation) + 1;
         }
         return theArray;
     }
@@ -34,6 +34,36 @@ public class SortAndSearch {
             if (i % 10 == 0) System.out.println();
             System.out.format("%4d ",array[i]);
         }
+    }
+    
+    static int[] merge(int a[], int b[]){
+        int m = a.length;
+        int n = b.length;
+        int aCount=0, bCount=0;
+        int i=0;
+        int c[] = new int[m+n];
+        
+        while(aCount<m && bCount<n){
+            if(a[aCount] < b[bCount]){
+                c[i] = a[aCount];
+                aCount++;
+            }
+            else{
+                c[i] = b[bCount];
+                bCount++;
+            }
+            i++;
+        }
+        
+        if(aCount==m){
+           for(int j=bCount; j<n; j++)
+               c[i++] = b[j];
+        }
+        else{
+           for(int j=aCount; j<m; j++)
+            c[i++] = a[j];
+        }
+        return c;
     }
     
     private static int[] myBubleSort(int[] us) {
@@ -126,26 +156,44 @@ public class SortAndSearch {
         return us;
     }// not working atm
     
-    private static int[] merge(int[] s1, int[] s2){
-        int s[] = new int[s1.length + s2.length];
-        int n1 = 0;
-        int n2 = 0;
-        for (int i = 0; i < s.length; i++){
-            if (n1 <= s1.length && n2 <= s2.length){
-                if (s1[n1] < s2[n2]){
+    private static int[] myMergeSort(int[] us){
+        //i = size of  first and following arrays to to merge
+        for(int i = 1; i*2 < us.length; i = i*2){
+            // current subarray
+            for(int n = 1 ; (n-1)*i*2 < us.length; n++){
+                System.out.println(2*i*(n-1));
+                System.out.println(i*(2*n-1));
+                System.out.println(i*(2*n-1));
+                System.out.println(i*n*2);
+                System.out.println("");
+                
+            
+                merge(Arrays.copyOfRange(us, 2*i*(n-1), i*(2*n-1)), Arrays.copyOfRange(us, i*(2*n-1), i*n*2));
+            }
+        }
+        return us;
+    }
+    
+    private static int[] myMerge(int[] s1, int[] s2){
+        int s[] = new int[s1.length+s2.length]; // creating new array with correct size
+        int n1 = 0; //current index of s1
+        int n2 = 0; //corrent index of s2
+        for (int i = 0; i < s.length; i++){ // loops trough every position in the new array
+            if (n1 < s1.length && n2 < s2.length){ //checking if n1 & n2 is within index range
+                if (s1[n1] < s2[n2]){  // choses lowest value, inserts in the s array and moving on to next index.
                     s[i] = s1[n1];
                     n1++;
                 }
-                else {
+                else { // choses lowest value, inserts in the s array and moving on to next index.
                     s[i] = s2[n2];
                     n2++;
                 }
             }
-            else if (n1 <= s1.length){
+            else if (n1 < s1.length){ // checking if n1 ia with in index range an adds index to s array.
                 s[i] = s1[n1];
                 n1++;
             }
-            else{
+            else{ // if you come here n2 is within index and is added to the s array.
                 s[i] = s2[n2];
                 n2++;
             }
@@ -154,19 +202,43 @@ public class SortAndSearch {
         return s;
     }
     
+    private static int[] myBogoSort(int[] us){
+        boolean correct = false;
+        while (!correct){
+            //shuffle
+            int r1 = (int)(Math.random()*us.length);
+            int r2 = (int)(Math.random()*us.length);
+            int temp = us[r1];
+            us[r1] = us[r2];
+            us[r2] = temp;
+            
+            //check
+            correct = true;
+            for (int i = 0; i < us.length - 1; i++){
+                if (us[i] < us[i+1]){
+                    correct = false;
+                }
+            }
+        }
+        return us;
+    }
+    
     public static void main(String[] args) {
         
-        int[] myArray = randomArray(100000);
-        printArray(myArray);
+        int[] myArray = randomArray(16 , 10); 
+//        printArray(myArray);
         
         Calendar clock11 = Calendar.getInstance();  
         long time11 = clock11.getTimeInMillis();
         
-//        myArray = mySelectionSort(myArray);
+   
+//        myArray = myBogoSort(myArray);
 //        myArray = myBubleSort(myArray);
-        myArray = myImprovedSort(myArray);
-//        myArray = splitSortS(myArray);
+//        myArray = mySelectionSort(myArray);
+//        myArray = myImprovedSort(myArray);
+        myArray = myMergeSort(myArray);
 //        Arrays.sort(myArray);
+        
         
         Calendar clock12 = Calendar.getInstance();  
         long time12 = clock12.getTimeInMillis();
@@ -174,7 +246,7 @@ public class SortAndSearch {
         printArray(myArray);
         
         System.out.println("");
-        System.out.println("time1 " + (time12-time11) + "ms"); 
+        System.out.println("time: " + (time12-time11) + "ms"); 
         
     }
 
