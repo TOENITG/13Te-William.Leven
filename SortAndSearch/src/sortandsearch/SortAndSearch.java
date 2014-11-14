7f98ff12d5bc9ebd3b3973d50989ce13ff3ce410
Gurgy <gurgy@emycia.com>
2014-11-14 09:44:37 +0100

    389234167-237

--- SortAndSearch/src/sortandsearch/SortAndSearch.java
+++ SortAndSearch/src/sortandsearch/SortAndSearch.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// doesent work because it can merge arrays with one element or subarray method is wrong
 
 package sortandsearch;
 
