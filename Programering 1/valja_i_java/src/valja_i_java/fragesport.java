87a79f4290e38067f5bc2b1cd87c908082de0e0d
Gurgy <gurgy@emycia.com>
2014-11-19 15:02:27 +0100

    re

--- Programering 1/valja_i_java/src/valja_i_java/fragesport.java
+++ Programering 1/valja_i_java/src/valja_i_java/fragesport.java
@@ -8,6 +8,32 @@ package valja_i_java;
 public class fragesport {
     
     public void run(){
+        // Predefined vars.
+        int points = 0;
+        String[] questions = {};
+        String[] answers = {};
+        String[] altanswers = {};
+        
+        // Loops trough questions.
+        for(int i = 0; i < 15; i++){
+            
+            // Grabbinging user input
+            String answer = "input";
+            
+            // checks if answer is correct
+            if( 9 < i && answer.equalsIgnoreCase(altanswers[i-10])){
+                points++;
+                //sho message gratz you get a point
+            }
+            else if(answer.equalsIgnoreCase(answers[i])){
+                points++;
+                //sho message gratz you get a point
+            }
+            else{
+                // show fail message
+            }
+            
+        }
         
     }
 }
