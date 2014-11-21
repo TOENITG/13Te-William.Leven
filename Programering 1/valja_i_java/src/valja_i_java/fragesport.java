84bfe0f5e9ef526a7e85100fa4f9330d5e3d7a8b
Gurgy <gurgy@emycia.com>
2014-11-21 10:35:15 +0100

    finnished uppg

--- Programering 1/valja_i_java/src/valja_i_java/fragesport.java
+++ Programering 1/valja_i_java/src/valja_i_java/fragesport.java
@@ -12,24 +12,25 @@ public class fragesport {
     public void run(){
         // Predefined vars with default values.
         int points = 0;
-        String[] questions = {"Price for one cup?", "Universe life and everything.", "Can eat a whole family pizza."};
-        String[] answers = {"5", "42", "Nils"};
-        String[] altanswers = {};
+        String[] questions = {"Price for one cup?", 
+            "Universe life and everything.", 
+            "Can eat a whole family pizza.", 
+            "Who is the author?" , 
+            "Creepers contains?", 
+            "Whitch game does the people on patrik's computer play?",
+            "Lain is .... of the web.",
+            "13Te consists of ... pupils."};
+        String[] answers = {"5", "42", "Nils", "William", "TNT", "bridge", "godess", "12"};
+        String[] altanswers = {"5kr", null, "Nils Grötlund", "William Leven", "dynamite", "cards", "emperror", "twelve"};
         
         // Loops trough questions.
-        for(int i = 0; i < 15; i++){
+        for(int i = 0; i < questions.length; i++){
             
             // Grabbinging user input
             String answer = JOptionPane.showInputDialog(null, questions[i], "Question nr " + (i+1) + "!", JOptionPane.QUESTION_MESSAGE);
             
             // checks if answer is correct
-            if( 9 < i && answer.equalsIgnoreCase(altanswers[i-10])){
-                points++;
-                JOptionPane.showMessageDialog(null, 
-                        "Your answer '" + answer + "' is correct and you have been rewarded one point. " + " Now you got " + points + " point(s).",
-                        questions[i], JOptionPane.INFORMATION_MESSAGE);
-            }
-            else if(answer.equalsIgnoreCase(answers[i])){
+            if(answer.equalsIgnoreCase(altanswers[i]) || answer.equalsIgnoreCase(answers[i])){
                 points++;
                 JOptionPane.showMessageDialog(null, 
                         "Your answer '" + answer + "' is correct and you have been rewarded one point. " + " Now you got " + points + " point(s).",
@@ -43,7 +44,7 @@ public class fragesport {
             
         }
         JOptionPane.showMessageDialog(null, 
-                        "Your final score is: " + points + " Point(s).",
+                        "Your final score is: " + points + "/" + questions.length + " Points.",
                         "Final score.", JOptionPane.INFORMATION_MESSAGE);
     }
 }
